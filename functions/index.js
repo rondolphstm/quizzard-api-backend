16d4a62e960c5754d5870976c65613ded618cbb5
rondolphstm <rondolphtst.m@gmail.com>
2022-09-11 20:02:14 -0400

    no more errors

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,22 @@
-const functions = require("firebase-functions");
+// import express and cors & etc.
+import express from "express"
+import cors from "cors"
+import functions from "firebase-functions"
+import { getPacks, addPacks} from "./src/routes.js"
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express()
+app.use(cors())
+app.use(express.json()) // will recieve json data
+// app.get('/home', (req,res)=>{
+//     res.send('gello world')
+//   })
+
+// app.get("/test", (req, res) => res.status(200).send("Here is my api !!!"))
+
+// app.get("/home", getPacks)
+app.get("/flashpack", getPacks)
+app.post("/createpack", addPacks)
+// app.patch("/updatepack", updatePack) add later
+// app.delete("/deletepack", deletePack) add later
+
+export const api = functions.https.onRequest(app)
